0173203f9c68a2d621efb32d88f55f10f3bb526d
sooraj-sky <ops@skywalks.in>
2023-03-05 09:41:00 +0530

    added lamda

--- terraform/rds-mysql.tf
+++ terraform/rds-mysql.tf
@@ -18,6 +18,65 @@ resource "aws_ssm_parameter" "rds_password" {
   overwrite = true
 }
 
+# Create a new VPC
+resource "aws_vpc" "mysql_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create public and private subnets within the VPC
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.mysql_vpc.id
+    cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.mysql_vpc.id
+    cidr_block = "10.0.2.0/24"
+}
+
+# Create an internet gateway and attach it to the VPC
+resource "aws_internet_gateway" "mysql_gateway" {
+    vpc_id = aws_vpc.mysql_vpc.id
+}
+
+# Create a route table for the public subnet
+resource "aws_route_table" "mysql_public_route" {
+    vpc_id = aws_vpc.mysql_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.mysql_gateway.id
+    }
+  
+}
+
+# Associate the public route table with the public subnet
+resource "aws_route_table_association" "mysql_public_association" {
+     subnet_id      = aws_subnet.public_subnet.id
+     route_table_id = aws_route_table.mysql_public_route.id
+  
+}
+
+# Create a DB subnet group for the private subnet
+resource "aws_db_subnet_group" "example" {
+  name        = "example-db-subnet-group"
+  description = "Subnet group for the RDS instance"
+  subnet_ids  = [aws_subnet.private_subnet.id]
+}
+
+# Create a security group for the RDS instance
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-sg-"
+  vpc_id      = aws_vpc.mysql_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.private_subnet.cidr_block]
+  }
+}
+
 # Create an RDS instance using the password retrieved from SSM
 resource "aws_db_instance" "mysqldb" {
   allocated_storage    = 10
@@ -31,4 +90,73 @@ resource "aws_db_instance" "mysqldb" {
   password             = aws_ssm_parameter.rds_password.value
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  subnet_group_name     = aws_db_subnet_group.example.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Owner       = "example"
+  }
+}
+
+
+# Create a Lambda function
+resource "aws_lambda_function" "example" {
+  filename         = "example.zip"
+  function_name    = "example-function"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "main.handler"
+  source_code_hash = filebase64sha256("example.zip")
+  runtime          = "python3.8"
+  timeout          = 60
+  memory_size      = 128
+
+  # Set environment variables to provide the RDS connection information
+  environment {
+    variables = {
+      RDS_HOST     = aws_db_instance.example.address
+      RDS_USERNAME = var.mysqluser
+      RDS_PASSWORD = aws_ssm_parameter.rds_password.value
+    }
+  }
+}
+
+# Grant the Lambda function permission to access the RDS instance
+resource "aws_lambda_permission" "example" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.example.arn
+}
+
+# Create a CloudWatch event rule to trigger the Lambda function
+resource "aws_cloudwatch_event_rule" "example" {
+  name        = "example-event-rule"
+  description = "Trigger Lambda function on schedule"
+
+  schedule_expression = "cron(0 12 * * ? *)"
+
+  # Trigger the Lambda function when the CloudWatch event occurs
+  target {
+    arn = aws_lambda_function.example.arn
+    id  = "example-target-id"
+  }
 }
