6c6e9b669752ecadb7728c43e03e0f836bb4a29d
sooraj-sky <ops@skywalks.in>
2023-03-05 10:19:37 +0530

    added lamda

--- terraform/rds-mysql.tf
+++ terraform/rds-mysql.tf
@@ -32,12 +32,12 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.mysql_vpc.id
     cidr_block = "10.0.2.0/24"
-     availability_zone = "us-east-1a"
+     availability_zone = "us-west-2c"
 }
 resource "aws_subnet" "private_subnet2" {
     vpc_id = aws_vpc.mysql_vpc.id
     cidr_block = "10.0.3.0/24"
-     availability_zone = "us-east-1b"
+     availability_zone = "us-west-2a"
 }
 
 resource "aws_subnet" "private_subnet3" {
